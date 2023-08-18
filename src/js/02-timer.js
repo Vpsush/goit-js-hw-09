55dbf686095757dae37b7949c07b793347b628ce
Vpsush <vpsush2023@gmail.com>
2023-08-18 23:35:39 +0300

    111

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,5 @@
 // import flatpickr from 'flatpickr';
-// import 'flatpickr/dist/flatpickr.min.css';
+import 'flatpickr/dist/flatpickr.min.css';
 
 const dataForm = document.querySelector('#datetime-picker');
 const buttonStart = document.querySelector('[data-start]');
@@ -7,6 +7,7 @@ const dataDays = document.querySelector('[data-days]');
 const dataHours = document.querySelector('[data-hours]');
 const dataMinutes = document.querySelector('[data-minutes]');
 const dataSeconds = document.querySelector('[data-seconds]');
+const timerClock = document.querySelector('.timer');
 
 const options = {
   enableTime: true,
@@ -18,32 +19,77 @@ const options = {
   },
 };
 
-buttonStart.addEventListener('click', onStart);
+buttonStart.addEventListener('click', () => {
+  timer.start();
+});
 
-function onStart() {
-  timerId = setInterval(startTimer, 1000);
-}
+// function onStart() {
+//   timerId = setInterval(startTimer, 1000);
+// }
 const timer = {
+  intervalId: null,
+  isActive: false,
   start() {
+    if (this.isActive) {
+      return;
+    }
     const startTime = Date.now();
+    this.isActive = true;
 
-    setInterval(() => {
-      const curetnTime = Date.now();
-      const deltaTime = curetnTime - startTime;
-      const timeComponents = getTimeComponents(deltaTime);
-      console.log(timeComponents);
+    this.intervalId = setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = currentTime - startTime;
+      const time = convertMs(deltaTime);
+      updateClockface(time);
+      dataDays.textContent = padDays(`${days}`);
+      dataHours.textContent = pad(`${hours}`);
+      dataMinutes.textContent = pad(`${minutes}`);
+      dataSeconds.textContent = pad(`${seconds}`);
     }, 1000);
   },
 };
-timer.start();
 
-function getTimeComponents(time) {
-  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const mins = Math.floor((time % (1000 * 60 * 24)) / (1000 * 60));
-  const secs = Math.floor((time % (1000 * 24)) / 1000);
-  return { hours, mins, secs };
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
+function padDays(value) {
+  return String(value).padStart(3, '0');
 }
 
-function updateClockface({ hours, mins, secs }) {
-  dataForm.clo;
+// function getTimeComponents(time) {
+//   const days = pad(
+//     Math.floor((time % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24))
+//   );
+//   const hours = pad(
+//     Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+//   );
+//   const mins = pad(Math.floor((time % (1000 * 60 * 24)) / (1000 * 60)));
+//   const secs = pad(Math.floor((time % (1000 * 24)) / 1000));
+//   return { days, hours, mins, secs };
+// }
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+function updateClockface({ days, hours, minutes, seconds }) {
+  timerClock.textContent = `${days}:${hours}:${minutes}:${seconds}`;
 }
