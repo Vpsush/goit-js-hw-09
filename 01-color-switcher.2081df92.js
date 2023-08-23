a4c5154699030542ca53cb7f2699d4db260f86ee
Vpsush <130247541+Vpsush@users.noreply.github.com>
2023-08-23 09:51:34 +0000

    Deploying to gh-pages from @ Vpsush/goit-js-hw-09@2084df86d9ddfac6e2a1f27194c9948f6d026f48 🚀

--- 01-color-switcher.2081df92.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body"),o=null;function r(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}t.addEventListener("click",(function(){o=setInterval(r,1e3),t.toggleAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(o)}))}();
-//# sourceMappingURL=01-color-switcher.2081df92.js.map
--- 01-color-switcher.44e2d845.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body");let o=null;function r(){n.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}t.addEventListener("click",(function(){o=setInterval(r,1e3),t.toggleAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(o)}));
-//# sourceMappingURL=01-color-switcher.44e2d845.js.map
